09a35e517ef7e581eb48a95d2e05bd0afe1d5b97
naylor <henriquenl09@gmail.com>
2022-01-01 17:28:41 -0300

    updated backend

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const express = require('express')
+const https = require('https')
+const DotEnv = require('dotenv').config()
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true})) // Body-parser Atual
 app.use(express.static(__dirname + '/public')) // Tem que colocar arquivos estáticos como HTML e CSS em uma pasta publica para poder usá-los
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/', (req, res) => {
+    const firstName = req.body.Fname
+    const lastName = req.body.Lname
+    const email = req.body.email
+
+    const data = { 
+        members: [{
+            email_address: email,
+            status: 'subscribed',
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            },
+        }
+    ]
+    }
+const jsonData = JSON.stringify(data)
+
+    const url = `https://us1.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`
+
+    const options = {
+        method: 'POST',
+        auth: `naylor:${process.env.MAILCHIMP_API_KEY}`
+    }
+
+ const request = https.request(url, options,(res) => {
+    response.on('data', (data) => {
+        console.log(JSON.parse(data))
+    })
+ })
+
+request.write(jsonData)
+request.end()
+
 })
 
 app.listen(port, () => {
